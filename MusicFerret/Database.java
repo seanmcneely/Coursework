7c7149cb7cb2e9d7093e9180a530b4c54dfc7467
seanmcneely <smcneely@usc.edu>
2020-07-07 16:25:36 -0400

    Update Database.java

--- MusicFerret/Database.java
+++ MusicFerret/Database.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
 public class Database {
 	String[] allGenres = {"Pop", "Rock", "Jazz", "Country", "Folk", "Blues", "Hip Hop", "Other"};
 	
-  //write a profile to MySQL database using profile object
 	public void createProfile(Profile profile) {
+		
 		Integer id = profile.getID();
 		String name = profile.getName();
 		String password = profile.getPassword();
@@ -35,11 +35,11 @@ public class Database {
 			connection = DriverManager.getConnection("jdbc:mysql://localhost/ferret?user=root&password=root");
 			statement = connection.prepareStatement(sql);
 			statement.setString(1,  id.toString());
-	    statement.setString(2, name);
-	    statement.setString(3, password);
-	    statement.setString(4, email);
-	    statement.setString(5, latitude.toString());
-	    statement.setString(6, longitude.toString());
+	        statement.setString(2, name);
+	        statement.setString(3, password);
+	        statement.setString(4, email);
+	        statement.setString(5, latitude.toString());
+	        statement.setString(6, longitude.toString());
 			statement.executeUpdate();
 			
 			sql = "INSERT INTO instrument_skill (profileID, instrumentName, skill) VALUES (?, ?, ?)";
@@ -47,9 +47,9 @@ public class Database {
 			
 			for(int i = 0; i < instruments.size(); i++) {
 				statement.setString(1, id.toString());
-			  statement.setString(2, instruments.get(i));
-			  statement.setString(3, skill.get(i).toString());
-			  statement.executeUpdate();
+			    statement.setString(2, instruments.get(i));
+			    statement.setString(3, skill.get(i).toString());
+			    statement.executeUpdate();
 			}
 			
 			sql = "INSERT INTO genre_rate (profileID, genreName, genreRating) VALUES (?, ?, ?)";
@@ -57,9 +57,9 @@ public class Database {
 			
 			for(int i = 0; i < allGenres.length; i++) {
 				statement.setString(1, id.toString());
-			  statement.setString(2, allGenres[i]);
-			  statement.setString(3, genres.get(i).toString());
-			  statement.executeUpdate();
+			    statement.setString(2, allGenres[i]);
+			    statement.setString(3, genres.get(i).toString());
+			    statement.executeUpdate();
 			}
 			
 		} catch(SQLException sqle) {
@@ -78,9 +78,11 @@ public class Database {
         }
 	}
 	
-  //returns all profiles in an list of profile objects
+	
 	public ArrayList<Profile> getAllProfiles(){
+		
 		ArrayList<Profile> profiles = new ArrayList<Profile>();
+		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
@@ -89,24 +91,24 @@ public class Database {
 		
 		PreparedStatement statement = null;
 		Connection connection = null;
-    ResultSet result = null;
+        ResultSet result = null;
         
-    String sql = "SELECT * FROM profile";
-    try {
+        String sql = "SELECT * FROM profile";
+        try {
 			connection = DriverManager.getConnection("jdbc:mysql://localhost/ferret?user=root&password=root");
 			statement = connection.prepareStatement(sql);
-		  result = statement.executeQuery();
-		  while(result.next()) {
+		    result = statement.executeQuery();
+		    while(result.next()) {
 		    	Integer id = null;
-				  String name = null;
-				  String password = null;
-				  String email = null;
-				  Location loc = null;
-				  Double latitude = null;
-				  Double longitude = null;
-				  ArrayList<String> instruments = new ArrayList<String>();
-				  ArrayList<Integer> genres = new ArrayList<Integer>();
-				  ArrayList<Integer> skill = new ArrayList<Integer>();
+				String name = null;
+				String password = null;
+				String email = null;
+				Location loc = null;
+				Double latitude = null;
+				Double longitude = null;
+				ArrayList<String> instruments = new ArrayList<String>();
+				ArrayList<Integer> genres = new ArrayList<Integer>();
+				ArrayList<Integer> skill = new ArrayList<Integer>();
 				
 		    	id = result.getInt("profileID");
 		    	name = result.getString("name");
@@ -117,59 +119,62 @@ public class Database {
 		    	loc = new Location(latitude, longitude);
 		    	
 		    	PreparedStatement instrumentStatement = null;
-		      ResultSet instrumentResult = null;
+		        ResultSet instrumentResult = null;
 		    	String instrument_sql = "SELECT * FROM instrument_skill WHERE profileID = ?";
 		    	instrumentStatement = connection.prepareStatement(instrument_sql);
-				  instrumentStatement.setString(1,  id.toString());
-				  instrumentResult = instrumentStatement.executeQuery();
-				  while(instrumentResult.next()) {
+				instrumentStatement.setString(1,  id.toString());
+				instrumentResult = instrumentStatement.executeQuery();
+				while(instrumentResult.next()) {
 					instruments.add(instrumentResult.getString("instrumentName"));
 					skill.add(Integer.parseInt(instrumentResult.getString("skill")));
 				}
 				
 				PreparedStatement genreStatement = null;
-		    ResultSet genreResult = null;
-		    String genre_sql = "SELECT * FROM genre_rate WHERE profileID = ? AND genreName = ?";
-		    genreStatement = connection.prepareStatement(genre_sql);
+		        ResultSet genreResult = null;
+		    	String genre_sql = "SELECT * FROM genre_rate WHERE profileID = ? AND genreName = ?";
+		    	genreStatement = connection.prepareStatement(genre_sql);
 		    	
-		    for(String genre : allGenres) {
+		    	for(String genre : allGenres) {
 		    		genreStatement.setString(1,  id.toString());
 		    		genreStatement.setString(2,  genre);
 		    		genreResult = genreStatement.executeQuery();
 		    		if(genreResult.next()) {
 		    			genres.add(Integer.parseInt(genreResult.getString("genreRating")));
-		        }
-		    }
-		    Profile user = new Profile(id, name, password, email, loc, instruments, genres, skill, true);
-		    profiles.add(user);
+		    		}
+		    	}
+		    	Profile user = new Profile(id, name, password, email, loc, instruments, genres, skill, true);
+		    	profiles.add(user);
 		    }
 		} catch (SQLException e) {
-			  e.printStackTrace();
-		}  
+			e.printStackTrace();
+		}
+        
 		return profiles;
 	}
 	
-	//queries database for user with matching email and returns profile object
+	
 	public Profile getProfileByEmail(String email) {
-	  Profile profile = null;
 		
-	  try {
+		Profile profile = null;
+		
+		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-	  } catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-	  }
+		}
 		
-  	PreparedStatement statement = null;
-	  Connection connection = null;
-    ResultSet result = null;
-    String sql = "SELECT * FROM profile WHERE email = ?";
-    try {
+		PreparedStatement statement = null;
+		Connection connection = null;
+        ResultSet result = null;
+        
+        String sql = "SELECT * FROM profile WHERE email = ?";
+        try {
 			connection = DriverManager.getConnection("jdbc:mysql://localhost/ferret?user=root&password=root");
 			statement = connection.prepareStatement(sql);
 			statement.setString(1, email);
-		  result = statement.executeQuery();
-		  if(result.next()) {
-		    Integer id = null;
+		    result = statement.executeQuery();
+		    if(result.next()) {
+		    	Integer id = null;
 				String name = null;
 				String password = null;
 				Location loc = null;
@@ -179,43 +184,42 @@ public class Database {
 				ArrayList<Integer> genres = new ArrayList<Integer>();
 				ArrayList<Integer> skill = new ArrayList<Integer>();
 				
-        id = result.getInt("profileID");
-		    name = result.getString("name");
-		    password = result.getString("password");
-		    latitude = result.getDouble("latitude");
-		    longitude = result.getDouble("longitude");
-		    loc = new Location(latitude, longitude);
+		    	id = result.getInt("profileID");
+		    	name = result.getString("name");
+		    	password = result.getString("password");
+		    	latitude = result.getDouble("latitude");
+		    	longitude = result.getDouble("longitude");
+		    	loc = new Location(latitude, longitude);
 		    	
-		    PreparedStatement instrumentStatement = null;
-		    ResultSet instrumentResult = null;
-		    String instrument_sql = "SELECT * FROM instrument_skill WHERE profileID = ?";
-		    instrumentStatement = connection.prepareStatement(instrument_sql);
+		    	PreparedStatement instrumentStatement = null;
+		        ResultSet instrumentResult = null;
+		    	String instrument_sql = "SELECT * FROM instrument_skill WHERE profileID = ?";
+		    	instrumentStatement = connection.prepareStatement(instrument_sql);
 				instrumentStatement.setString(1,  id.toString());
 				instrumentResult = instrumentStatement.executeQuery();
-        
 				while(instrumentResult.next()) {
 					instruments.add(instrumentResult.getString("instrumentName"));
 					skill.add(Integer.parseInt(instrumentResult.getString("skill")));
 				}
 				
 				PreparedStatement genreStatement = null;
-		    ResultSet genreResult = null;
-		    String genre_sql = "SELECT * FROM genre_rate WHERE profileID = ? AND genreName = ?";
-		    genreStatement = connection.prepareStatement(genre_sql);
+		        ResultSet genreResult = null;
+		    	String genre_sql = "SELECT * FROM genre_rate WHERE profileID = ? AND genreName = ?";
+		    	genreStatement = connection.prepareStatement(genre_sql);
 		    	
-		    for(String genre : allGenres) {
-		    	genreStatement.setString(1,  id.toString());
-		    	genreStatement.setString(2,  genre);
-		    	genreResult = genreStatement.executeQuery();
-		    	if(genreResult.next()) {
-		    		genres.add(Integer.parseInt(genreResult.getString("genreRating")));
+		    	for(String genre : allGenres) {
+		    		genreStatement.setString(1,  id.toString());
+		    		genreStatement.setString(2,  genre);
+		    		genreResult = genreStatement.executeQuery();
+		    		if(genreResult.next()) {
+		    			genres.add(Integer.parseInt(genreResult.getString("genreRating")));
+		    		}
 		    	}
+		    	profile = new Profile(id, name, password, email, loc, instruments, genres, skill, true);
 		    }
-		    profile = new Profile(id, name, password, email, loc, instruments, genres, skill, true);
-		   }
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-    return profile;
+        return profile;
 	}
 }
