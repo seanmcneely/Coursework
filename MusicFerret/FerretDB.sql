05645942518231d078252849c91a280f03c82395
seanmcneely <smcneely@usc.edu>
2020-07-07 16:43:26 -0400

    Update FerretDB.sql

--- MusicFerret/FerretDB.sql
+++ MusicFerret/FerretDB.sql
@@ -10,11 +10,11 @@ CREATE TABLE Profile (
     password VARCHAR(45) NOT NULL,
     email VARCHAR(45) NOT NULL,
     latitude DOUBLE(12,9 ) NOT NULL,
-	longitude Double(12, 9) NOT NULL
+    longitude Double(12, 9) NOT NULL
 );
 
 CREATE TABLE instrument_skill(
-	  instrumentID INT(11) PRIMARY KEY AUTO_INCREMENT,
+    instrumentID INT(11) PRIMARY KEY AUTO_INCREMENT,
     profileID INT(11) NOT NULL,
     instrumentName VARCHAR(45) NOT NULL,
     skill INT(2) NOT NULL,
@@ -22,9 +22,9 @@ CREATE TABLE instrument_skill(
 );
 
 CREATE TABLE genre_rate(
-	  genreID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    genreID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     profileID INT(11) NOT NULL,
     genreName varchar(50) NOT NULL,
     genreRating INT(2) NOT NULL,
-	  FOREIGN KEY (profileID) REFERENCES Profile(profileID)
+    FOREIGN KEY (profileID) REFERENCES Profile(profileID)
 );
